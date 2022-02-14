080c50917ed6fa8aed10a4f4612178c3522dd8de
Pr0lamer <andrewmakmail@gmail.com>
2022-02-14 18:29:04 +0200

    calculatin and printing the results

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 let resultText = document.getElementsByClassName("result-text");
 let userInput = document.getElementById("user-input");
 
+let lengthEl = resultText[0];
+let volumeEl = resultText[1];
+let massEl = resultText[2];
+
 const feetPerMeter = 3.28084;
 const gallonPerLiter = 0.264172;
-const kilkilogramPerPounts = 2.2;
-
-let currentNumber = 0;
-
+const kilogramPerPounts = 2.2;
 
 userInput.addEventListener("input", function(event){
-    currentNumber = event.target.value;
-    if (event.target.value == '') {
-        event.target.value = 0
-      }
+    let currentNumber = event.target.value;
+    let firstResult = 0;
+    let secondResult = 0
+
+    /*if (event.target.value == '') {
+        event.target.value = 0;
+    }*/
+
     console.log(currentNumber);
+
+    firstResult = Math.floor(currentNumber * feetPerMeter * 1000) / 1000;
+    secondResult = Math.floor(currentNumber / feetPerMeter * 1000) / 1000;
+    lengthEl.textContent = `${currentNumber} meters = ${firstResult} feet | ${currentNumber} feet = ${secondResult} meters`;
+
+
+    firstResult = Math.floor(currentNumber * gallonPerLiter * 1000) / 1000;
+    secondResult = Math.floor(currentNumber / gallonPerLiter * 1000) / 1000;
+    volumeEl.textContent = `${currentNumber} liters = ${firstResult} gallons | ${currentNumber} gallons = ${secondResult} liters`;
+
+    firstResult = (Math.floor(currentNumber * kilogramPerPounts * 1000) / 1000);
+    secondResult = Math.floor(currentNumber / kilogramPerPounts * 1000) / 1000;
+    massEl.textContent = `${currentNumber} kilos = ${firstResult} pounds | ${currentNumber} pounds = ${secondResult} kilos`;
 });
 
-console.log(userInput.textContent);
-let lengthEl = resultText[0];
-let volumehEl = resultText[1];
-let massEl = resultText[2];
+
+
